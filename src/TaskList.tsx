465f950f3d6086723d25e80c30e78f0ac3db025d
Hm | edihasam <masa.ishi.right@gmail.com>
2019-02-12 11:40:05 +0900

    press enter then create new task

--- src/TaskList.tsx
+++ src/TaskList.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import Task from "./Task";
+import TaskListItem from "./TaskListItem";
 import { ITasks, tasks } from "./TasksData";
 
 interface IState {
@@ -22,11 +22,23 @@ class TaskList extends React.Component<{}, IState> {
     return (
       <div className="border border-gey-lighter my-4 py-6 w-3/5 h-full mx-auto">
         {this.state.tasks.map(task => (
-          <Task key={task.id} title={task.title} />
+          <TaskListItem
+            key={task.id}
+            title={task.title}
+            handleEnterKeyDown={this.newTaskCreate}
+          />
         ))}
       </div>
     );
   }
+
+  private newTaskCreate = () => {
+    const id = this.state.tasks.slice(-1)[0].id + 1;
+    const title = "something to do";
+    const newTasks = this.state.tasks.slice();
+    newTasks.push({ id, title });
+    this.setState({ tasks: newTasks });
+  };
 }
 
 export default TaskList;
--- src/TasksData.ts
+++ src/TasksData.ts
@@ -1,28 +1,23 @@
 export interface ITasks {
   id: number;
   title: string;
-  completed: boolean;
 }
 
 export const tasks: ITasks[] = [
   {
     id: 1,
-    title: "something to do",
-    completed: false
+    title: "something to do"
   },
   {
     id: 2,
-    title: "anything to do",
-    completed: true
+    title: "anything to do"
   },
   {
     id: 3,
-    title: "everything to do",
-    completed: false
+    title: "everything to do"
   },
   {
     id: 4,
-    title: "nothing to do",
-    completed: false
+    title: "nothing to do"
   }
 ];
