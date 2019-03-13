1cff734dfcfdef6110e02cc398e49d8ce71a1b8e
Hm | edihasam <masa.ishi.right@gmail.com>
2019-03-13 09:56:17 +0900

    [update] sync initial currentTask with focused item

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Header from "./Header";
 import Tasks from "./Tasks";
